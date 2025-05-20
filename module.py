65a3e0499ca34010695ffb41022e93efab7145a9
Arpita <arpitakumari10653@gmail.com>
2025-05-20 19:25:16 +0530

    how to comment

--- module.py
+++ module.py
@@ -1,4 +1,13 @@
 import pyjokes
 
+# print ("Printing jokes...")
+
+# This prints a random joke
 joke = pyjokes.get_joke()
 print(joke)
+
+
+# so thanks
+# that was my program
+# another line
+# yet another line
